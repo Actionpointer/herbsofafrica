69ae79afab387b9363277f28ef57a6201466bf75
dreigningking <reigningkingforever@gmail.com>
2024-05-13 13:36:17 +0100

    change automatic payout schedule to every monday

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new AffiliatePaymentsJob)->hourly();
+        $schedule->job(new AffiliatePaymentsJob)->weeklyOn(1, '8:00');
         $schedule->job(new AffiliatePaymentsVerificationJob)->hourly();
     }
 
